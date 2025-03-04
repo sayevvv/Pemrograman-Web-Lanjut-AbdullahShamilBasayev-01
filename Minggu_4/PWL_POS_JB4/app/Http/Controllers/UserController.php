dad5b5af2984d92b3fad01c6ed05785e6a67d35a
sayevvv <shamilbasayev10@gmail.com>
2025-03-04 08:49:47 +0700

    Jobsheet 4 Praktikum 2.3

--- Minggu_4/PWL_POS_JB4/app/Http/Controllers/UserController.php
+++ Minggu_4/PWL_POS_JB4/app/Http/Controllers/UserController.php
@@ -53,7 +53,10 @@ class UserController extends Controller
 
         // PRAKTIKUM 2.2
         // $user = UserModel::findOrFail(1);
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // $user = UserModel::where('username', 'manager9')->firstOrFail();
+
+        $user = UserModel::where('level_id', 2)->count();
+        // dd($user);
         return view('user', ['data' => $user]);
     }
 }
--- Minggu_4/PWL_POS_JB4/resources/views/user.blade.php
+++ Minggu_4/PWL_POS_JB4/resources/views/user.blade.php
@@ -7,16 +7,17 @@
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <th>ID</th>
-            <th>Username</th>
+            <th>Jumlah ID</th>
+            {{-- <th>Username</th>
             <th>Nama</th>
-            <th>Level Pengguna</th>
+            <th>Level Pengguna</th> --}}
         </tr>
         <tr>
-            <td>{{ $data->user_id }}</td>
-            <td>{{ $data->username }}</td>
+            <td>{{ $data }}</td>
+            {{-- <td>{{ $data->user_id }}</td> --}}
+            {{-- <td>{{ $data->username }}</td>
             <td>{{ $data->nama }}</td>
-            <td>{{ $data->level_id }}</td>
+            <td>{{ $data->level_id }}</td> --}}
         </tr>
     </table>
 </body>
