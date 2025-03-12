2c58d8c3c0fe81d9219f8ca1cb87156c2dd291da
sayevvv <shamilbasayev10@gmail.com>
2025-03-12 08:59:25 +0700

    Jobsheet 4 Done :fire: :rocket:

--- Minggu_4/PWL_POS_JB4/app/Http/Controllers/UserController.php
+++ Minggu_4/PWL_POS_JB4/app/Http/Controllers/UserController.php
@@ -143,6 +143,23 @@ class UserController extends Controller
         return view('user_tambah');
     }
 
+    public function ubah($id)
+    {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+
+    public function ubah_simpan(Request $request, $id)
+    {
+        $user = UserModel::find($id);
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make($request->password);
+        $user->level_id = $request->level_id;
+        $user->save();
+        return redirect('/user');
+    }
+
     public function tambah_simpan(Request $request)
     {
         $data = [
