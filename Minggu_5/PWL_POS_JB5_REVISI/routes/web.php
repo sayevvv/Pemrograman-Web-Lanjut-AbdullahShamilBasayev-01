058323ec54bd3340a52f1cee0d41db141ba588c1
sayevvv <shamilbasayev10@gmail.com>
2025-03-22 20:37:07 +0700

    Jobsheet 5 Praktikum 2 :rocket:

--- Minggu_5/PWL_POS_JB5_REVISI/routes/web.php
+++ Minggu_5/PWL_POS_JB5_REVISI/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\KategoriController;
 
 /*
@@ -16,9 +17,9 @@ use App\Http\Controllers\KategoriController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
@@ -28,3 +29,6 @@ Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
 Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
 Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+
+// JOBSHEET 5 Praktikum 2
+Route::get('/', [WelcomeController::class, 'index']);
