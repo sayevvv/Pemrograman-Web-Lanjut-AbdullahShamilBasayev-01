1b89b5b741014458517544eeaecfb2fda5daf352
sayevvv <shamilbasayev10@gmail.com>
2025-03-23 21:29:55 +0700

    Jobsheet 6 Praktikum 2

--- Minggu_6/PWL_POS_JB6/routes/web.php
+++ Minggu_6/PWL_POS_JB6/routes/web.php
@@ -41,13 +41,16 @@ Route::group(['prefix' => 'user'], function () {
     Route::post('/list', [UserController::class, 'list']);     // menampilkan data user dalam bentuk json untuk datatables
     Route::get('/create', [UserController::class, 'create']);  // menampilkan halaman form tambah user
     Route::post('/', [UserController::class, 'store']);        // menyimpan data user baru
+    // JOBSHEET 6 PRAKTIKUM 1
     Route::get('/create_ajax', [UserController::class, 'create_ajax']); // menampilkan halaman form tambah user dengan ajax
     Route::post('/ajax', [UserController::class, 'store_ajax']);   // menyimpan data user baru dengan ajax
+    // JOBSHEET 6 PRAKTIKUM 2
+    Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']); // menampilkan halaman form edit user dengan ajax
+    Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']);   // menyimpan perubahan data user dengan ajax
     Route::get('/{id}', [UserController::class, 'show']);      // menampilkan detail user
     Route::get('/{id}/edit', [UserController::class, 'edit']); // menampilkan halaman form edit user
     Route::put('/{id}', [UserController::class, 'update']);    // menyimpan perubahan data user
     Route::delete('/{id}', [UserController::class, 'destroy']);// menghapus data user
-    // JOBSHEET 6 PRAKTIKUM 1
 });
 
 // JOBSHEET 5 TUGAS PRAKTIKUM
