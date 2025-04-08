defdff509f72b8d07daa763b8c89a82a4d5c5200
sayevvv <shamilbasayev10@gmail.com>
2025-04-08 19:20:21 +0700

    Implementasi JS-3: Query Builder; Praktikum ini hanya untuk percobaan pada tabel kategori, karena tidak signifikan pada hasil akhir project ini

--- UTS/UTS_POS/app/Http/Controllers/KategoriController.php
+++ UTS/UTS_POS/app/Http/Controllers/KategoriController.php
@@ -9,7 +9,15 @@ class KategoriController extends Controller
 {
     public function index()
     {
-        $data = DB::select('select * from m_kategori');
-        return view('kategori.index', ['data' => $data]);
+        $data = [
+            'kategori_kode' => 'SNK',
+            'kategori_nama' => 'Suku Cadang',
+            'created_at' => now(),
+        ];
+        DB::table('m_kategori')->insert($data);
+        $tes = DB::table('m_kategori')->get();
+        return view('kategori.index', ['data' => $tes]);
+        // $data = DB::select('select * from m_kategori');
+        // return view('kategori.index', ['data' => $data]);
     }
 }
