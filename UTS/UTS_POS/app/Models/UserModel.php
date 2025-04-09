1e0ce74eabe9d534cf133cfc0600c2e2efb9e183
sayevvv <shamilbasayev10@gmail.com>
2025-04-10 06:47:50 +0700

    Implementasi JS-7: Authorization and MultiAuthorization; Menambahkan fitur authorization, tiap role memiliki batasan tersendiri

--- UTS/UTS_POS/app/Models/UserModel.php
+++ UTS/UTS_POS/app/Models/UserModel.php
@@ -32,4 +32,17 @@ class UserModel extends Authenticatable
     {
         return $this->hasMany(StokModel::class, 'user_id', 'user_id');
     }
+    // Mendapatkan nama role
+    public function getRoleName(): string
+    {
+        return $this->level->level_nama;
+    }
+    // Cek apakah user memiliki role tertentu
+    public function hasRole(string $role): bool
+    {
+        return $this->level->level_kode === $role;
+    }
+    public function getRole() {
+        return $this->level->level_kode;
+    }
 }
