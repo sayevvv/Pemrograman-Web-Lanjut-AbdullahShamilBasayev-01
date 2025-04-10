89e5481e8c77f7734e5f13efa70fbafa7376c942
sayevvv <shamilbasayev10@gmail.com>
2025-04-10 16:49:00 +0700

    Implementasi JS-8: Profile; Membuat tempat sortir pfp hasil upload dalam storage/

--- UTS/UTS_POS/routes/web.php
+++ UTS/UTS_POS/routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\StokController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\BarangController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\SupplierController;
