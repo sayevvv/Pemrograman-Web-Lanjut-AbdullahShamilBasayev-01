5a46e2849e8e02bc26b833704bc80fc3349725aa
sayevvv <shamilbasayev10@gmail.com>
2025-04-23 15:47:01 +0700

    Praktikum 3 - Membuat RESTful API Logout

--- Minggu_10/PWL_POS_JB10/routes/api.php
+++ Minggu_10/PWL_POS_JB10/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
 use App\Http\Controllers\Api\RegisterController;
 
 /*
@@ -18,6 +19,7 @@ use App\Http\Controllers\Api\RegisterController;
 
 Route::post('/register', RegisterController::class)->name('register');
 Route::post('/login', LoginController::class)->name('login');
+Route::post('/logout', LogoutController::class)->name('logout');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
